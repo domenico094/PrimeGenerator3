6e40f94c0a14996d54fc7fe54f7df9e6e35f53fe
Domenico <domy-and94@outlook.com>
2020-11-04 00:31:21 +0100

    Ho Testato entrambi i metodi

--- src/test/java/it/unical/PrimeGenerator3/MyMathTest.java
+++ src/test/java/it/unical/PrimeGenerator3/MyMathTest.java
@@ -1,7 +1,10 @@
 package it.unical.PrimeGenerator3;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 
 import org.junit.BeforeClass;
@@ -19,5 +22,14 @@ public class MyMathTest {
 	@Test
 	public void primeNumbersGeneratorWorks()	{
 		assertEquals(new LinkedList<Integer>(),math.primeNumbersGenerator(1));
+		assertEquals(Arrays.asList(2,3,5,7),math.primeNumbersGenerator(7));
+		
+	}
+	
+	@Test 
+	public void isPrime()
+	{
+		assertTrue(math.isPrime(7));
+		assertFalse(math.isPrime(8));
 	}
 }
